6263983ae72622e4c9b4fd80cce615d3f78b4428
Your Name <you@example.com>
2020-01-03 18:27:26 +0800

    add egrep

--- sysbench.sh
+++ sysbench.sh
@@ -8,6 +8,6 @@ read -p "other comment (serve config):" other
 echo "test of $con config,$tn tables, size:$ts, threads:$tr, $t1 second at $other $tt" >> ~/mysql.log 
 mysql -uroot -e "create database testdb"
 sysbench --mysql-host=127.0.0.1 --mysql-user=root  --mysql-db=testdb --report-interval=10 --time=$t1 --tables=$tn --table-size=$ts --threads=$tr /usr/share/sysbench/oltp_read_write.lua prepare
-sysbench --mysql-host=127.0.0.1 --mysql-user=root  --mysql-db=testdb --report-interval=10 --time=$t1 --tables=$tn --table-size=$ts --threads=$tr /usr/share/sysbench/oltp_read_write.lua run
+sysbench --mysql-host=127.0.0.1 --mysql-user=root  --mysql-db=testdb --report-interval=10 --time=$t1 --tables=$tn --table-size=$ts --threads=$tr /usr/share/sysbench/oltp_read_write.lua run |egrep "transactions|queries" >> ~/mysql.log
 sysbench --mysql-host=127.0.0.1 --mysql-user=root  --mysql-db=testdb --report-interval=10 --time=$t1 --tables=$tn --table-size=$ts --threads=$tr /usr/share/sysbench/oltp_read_write.lua cleanup
 
