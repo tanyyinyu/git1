d65fd099068af2709ecccaf1141f1d9e86d3e8b8
Your Name <you@example.com>
2020-01-03 17:28:11 +0800

    ch sysbench

--- sysbench.sh
+++ sysbench.sh
@@ -6,7 +6,7 @@ read -p "please input test threads numbers:" tr
 read -p "please input test time(120 or other):" t1
 read -p "other comment (serve config):" other
 echo "test of $con config,$tn tables, size:$ts, threads:$tr, $t1 second at $other" >> ~/mysql.log 
-cp -r /data/mysql /data/mysql.bak
+#cp -r /data/mysql /data/mysql.bak
 mysql -uroot -e "create database testdb"
 sysbench --mysql-host=127.0.0.1 --mysql-user=root  --mysql-db=testdb --report-interval=10 --time=$ti --tables=$tn --table-size=$ts --threads=$tr /usr/share/sysbench/oltp_read_write.lua prepare
 sysbench --mysql-host=127.0.0.1 --mysql-user=root  --mysql-db=testdb --report-interval=10 --time=$ti --tables=$tn --table-size=$ts --threads=$tr /usr/share/sysbench/oltp_read_write.lua run
