6fe1b65be59e81c69e07da918be5fbe1f5e57f31
Your Name <you@example.com>
2020-01-03 18:08:29 +0800

    ch cleansh

--- cleantest.sh
+++ cleantest.sh
@@ -1,7 +1,7 @@
 pkill mysql
-rm -rf /data/mysql
-/usr/bin/cp -r /data/mysql.bak/ /data/mysql
 sync 
-service mysqld start
+service mysqld restart
+mysql -uroot < /root/newdump.sql
+
 #echo 3 >/proc/sys/vm/drop_cache #云机器不能清除缓存;
 
