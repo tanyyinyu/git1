e9d9af1216da6d4d4a70ff87141309a09d584181
tanyyinyu <59174177+tanyyinyu@users.noreply.github.com>
2020-02-25 23:37:07 +0800

    Update sysbench.sh

--- sysbench.sh
+++ sysbench.sh
@@ -1,15 +1,88 @@
 #! /bin/bash
-tt=`date`
-read -p "please input test config (small,medium,large,huge):" con
-read -p "please input test table numbers:" tn
-read -p "please input test table size:" ts 
-read -p "please input test threads numbers:" tr
-read -p "please input test time(120 or other):" t1
-read -p "other comment (serve config):" other
-echo "test of $con config,$tn tables, size:$ts, threads:$tr, $t1 second at $other $tt" >> ~/mysql.log 
-mysql -uroot -e "create database testdb"
-sysbench --mysql-host=127.0.0.1 --mysql-user=root  --mysql-db=testdb --report-interval=10 --time=$t1 --tables=$tn --table-size=$ts --threads=$tr /usr/share/sysbench/oltp_read_write.lua prepare
-sysbench --mysql-host=127.0.0.1 --mysql-user=root  --mysql-db=testdb --report-interval=10 --time=$t1 --tables=$tn --table-size=$ts --threads=$tr /usr/share/sysbench/oltp_read_write.lua run |egrep "transactions|queries" >> ~/mysql.log
-sysbench --mysql-host=127.0.0.1 --mysql-user=root  --mysql-db=testdb --report-interval=10 --time=$t1 --tables=$tn --table-size=$ts --threads=$tr /usr/share/sysbench/oltp_read_write.lua cleanup
-mysql -uroot -e "drop database testdb"
-tail -5  ~/mysql.log
+
+#definations
+DATE=`date +%Y-%m-%d`
+LOG=/root/"$DATE"sysbench.log
+
+### need to be updated;
+#mysql
+SQL_ID=root    
+SQL_PW=Friday28
+SQL_HOST=127.0.0.1  
+SQL_PORT=3306
+
+#sysbench
+TEST_LUA=/usr/share/sysbench/oltp_read_write.lua
+TIME=60
+T_NUM=4
+T_SIZE=10000
+THREADS=50
+
+#t2 run times
+T2=1
+
+info () {
+  lscpu |grep "CPU(s):" >> $LOG ;lscpu |grep "Thread" >> $LOG; lscpu |grep "L[0-9]" >> $LOG
+  head -1 /proc/meminfo >> $LOG
+  echo "--time=$TIME --tables=$T_NUM --table-size=$T_SIZE --threads=$THREADS $TEST_LUA $T2 times" >> $LOG
+}
+
+check_sysbench () {
+  if which sysbench
+    then 
+	echo "ok"
+    else
+  	curl -s https://packagecloud.io/install/repositories/akopytov/sysbench/script.rpm.sh | sudo bash
+	sudo yum -y install sysbench	
+  fi
+}
+
+check_mysql () {
+  if which mysql
+    then
+	echo "ok"
+    else
+	echo "there is no mysql or no links for it"
+        exit
+  fi
+}
+
+#prepare
+prepare () {
+  mysql -u$SQL_ID -p$SQL_PW -h$SQL_HOST -P$SQL_PORT -e "drop database testdb"
+  mysql -u$SQL_ID -p$SQL_PW -h$SQL_HOST -P$SQL_PORT -e "create database testdb" 
+}
+
+clean () {
+  mysql -u$SQL_ID -p$SQL_PW -h$SQL_HOST -P$SQL_PORT -e "drop database testdb"
+  sync 
+  service mysqld restart
+}
+
+show () {
+  tail $LOG
+}
+
+average () {
+    for TRANS in `tail -$(($T2*10)) $LOG |grep "transaction"|awk '{print $2}'`
+	do
+	  SUM=$((SUM+TRANS)) 
+ 	 done
+    echo $SUM
+    echo "average Transaction is $(($SUM/$T2))"
+    echo "average Transaction is $(($SUM/$T2))" >> $LOG
+}
+
+for i in `seq 1 $T2`
+do
+  info
+  check_sysbench
+  check_mysql
+  prepare
+  sysbench --mysql-host=$SQL_HOST --mysql-port=$SQL_PORT --mysql-user=$SQL_ID --mysql-password=$SQL_PW --mysql-db=testdb --report-interval=10  --time=$TIME --tables=$T_NUM --table-size=$T_SIZE --threads=$THREADS     $TEST_LUA prepare
+  sysbench --mysql-host=$SQL_HOST --mysql-port=$SQL_PORT --mysql-user=$SQL_ID --mysql-password=$SQL_PW --mysql-db=testdb --report-interval=10  --time=$TIME --tables=$T_NUM --table-size=$T_SIZE --threads=$THREADS     $TEST_LUA run |egrep "transactions|queries" >> $LOG
+  sysbench --mysql-host=$SQL_HOST --mysql-port=$SQL_PORT --mysql-user=$SQL_ID --mysql-password=$SQL_PW --mysql-db=testdb --report-interval=10  --time=$TIME --tables=$T_NUM --table-size=$T_SIZE --threads=$THREADS     $TEST_LUA cleanup
+  clean
+  #show
+done
+average
